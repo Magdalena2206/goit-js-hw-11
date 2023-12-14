ecdd18def668d542bac3af577917a72ce9a5b709
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-12-14 20:47:35 +0100

    add elements js

---
+++ src/index.js
@@ -0,0 +1,99 @@
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+import { API_URL, options }  from './api.js';
+import Notiflix from 'notiflix';
+import { elements } from './elements';
+
+
+const { galleryEl, searchInput, searchForm, loaderEl } = elements;
+const lightbox = new SimpleLightbox('.lightbox', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    enableKeyboard: true,
+    showCounter: false,
+    scrollZoom: false,
+    close: false,
+  });
+let totalHits = 0;
+let isLoadingMore = false;
+let reachedEnd = false;
+
+searchForm.addEventListener('submit', onFormSubmit);
+window.addEventListener('scroll', onScrollHandler);
+document.addEventListener('DOMContentLoaded', hideLoader);
+
+function showLoader() {
+    loaderEl.style.display = 'block';
+}
+
+function hideLoader() {
+    loaderEl.style.display = 'none';
+}
+
+function renderGallery(hits) {
+    const markup = hits
+        .map(item => {
+            return `<a href="${item.largeImageURL}" class="lightbox">
+        <div class="photo-card">
+            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b>
+                    ${item.likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b>
+                    ${item.views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b>
+                    ${item.comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b>
+                    ${item.downloads}
+                </p>
+            </div>
+        </div>
+    </a>`;
+        })
+        .join('');
+    galleryEl.insertAdjacentHTML('beforeend', markup);
+    if (options.params.page * options.params.per_page >= totalHits) {
+        if (!reachedEnd) {
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            reachedEnd = true;
+        }
+    }
+    lightbox.refresh();
+}
+
+async function loadMore() {
+    isLoadingMore = true;
+    options.params.page += 1;
+    try {
+        showLoader();
+        const response = await axios.get(API_URL, options);
+        const hits = response.data.hits;
+        renderGallery(hits);
+      } catch (err) {
+        Notiflix.Notify.failure(err);
+        hideLoader();
+      } finally {
+        hideLoader();
+        isLoadingMore = false; 
+    }
+}
+
+function onScrollHandler() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const scrollThereshold = 300;
+    if (scrollTop + clientHeight >= scrollHeight - scrollThereshold &&
+        galleryEl.innerHTML !== '' &&
+        !isLoadingMore &&
+        !reachedEnd) {
+        loadMore();
+        }
+}
